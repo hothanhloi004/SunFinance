0fe7591ad5432044fb3527cd1c8cb39a64d5ae88
Dat <2224802010798@student.tdmu.edu.vn>
2026-02-27 23:51:47 +0700

    Transaction Do 0.0.3

--- FinTrack/app/build.gradle.kts
+++ FinTrack/app/build.gradle.kts
@@ -40,4 +40,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    val cameraxVersion = "1.3.1"
+
+    implementation("androidx.camera:camera-core:$cameraxVersion")
+    implementation("androidx.camera:camera-camera2:$cameraxVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
+    implementation("androidx.camera:camera-view:$cameraxVersion")
 }
--- FinTrack/app/src/main/java/com/example/fintrack/TransactionService/ScanReceiptActivity.java
+++ FinTrack/app/src/main/java/com/example/fintrack/TransactionService/ScanReceiptActivity.java
@@ -1,13 +1,19 @@
 package com.example.fintrack.TransactionService;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.fintrack.R;
 
 public class ScanReceiptActivity extends AppCompatActivity {
 
+    private static final int CAMERA_REQUEST_CODE = 100;
     TextView txtTotalAmount;
 
     @Override
@@ -16,6 +22,15 @@ public class ScanReceiptActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scan_receipt);
 
         txtTotalAmount = findViewById(R.id.txtTotalAmount);
+
+        // ✅ Xin quyền camera đúng chỗ
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    CAMERA_REQUEST_CODE);
+        }
 
         // Giả lập dữ liệu OCR trả về
         updateTotalAmount(55000);
