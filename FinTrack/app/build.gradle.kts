dd0aa7bffeda65f5927c2bab37443dd2b279df38
hothanhloi <hothanhloi595@gmail.com>
2026-03-05 12:29:15 +0700

    Transaction 0.0.2

--- FinTrack/app/build.gradle.kts
+++ FinTrack/app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     implementation(libs.appcompat)
